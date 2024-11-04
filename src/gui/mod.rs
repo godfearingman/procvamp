5ae7f535f1145f62b38c9812b9dadb6d4e9a38f8
dx9 <dx9@cultofintellect.com>
2024-11-04 16:31:57 +0000

    boiler

---
+++ src/gui/mod.rs
@@ -0,0 +1,28 @@
+pub mod gui {
+    // Represent all types of windows as an enum
+    //
+    pub enum WindowType {
+        DisassemblyView,
+        FunctionView,
+        ScannerView,
+        ScannerResults,
+        GraphView,
+    }
+    // Basic struct for defining windows and what type they are
+    //
+    pub struct Window {
+        /// Our window name
+        ///
+        name: String,
+        /// Our window type
+        ///
+        win_type: WindowType,
+    }
+    // Define our implementation of this struct, all we'll need is a construtor
+    //
+    impl Window {
+        pub fn new(name: String, win_type: WindowType) -> Self {
+            Self { name, win_type }
+        }
+    }
+}
